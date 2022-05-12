31c47eac07f24a93477e1af38e15f4e29be42bbb
Amar Maruf <amarmaruf2403@gmail.com>
2022-05-12 23:44:04 +0700

    menghapus web component dan mengembalikan index.html

---
+++ pages/dataPenduduk.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no" />
+  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/favicon.png" />
+  <link rel="icon" type="image/png" href="../assets/img/favicon.png" />
+  <title>Halaman Dashboard | Data Penduduk</title>
+  <!-- font and icons -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700,900|Roboto+Slab:400,700" />
+  <!-- nucleo Icons -->
+  <link rel="stylesheet" href="../assets/css/nucleo-icons.css" />
+  <link rel="stylesheet" href="../assets/css/nucleo-svg.css" />
+  <!-- font Awesome Icons -->
+  <script src="https://kit.fontawesome.com/17bdaf7867.js" crossorigin="anonymous"></script>
+  <!-- Material Icons -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Round" rel="stylesheet">
+  <!-- CSS Files -->
+  <link id="pagestyle" href="../assets/css/material-dashboard.css?v=3.0.2" rel="stylesheet" />
+  <!-- web Component Custom -->
+  <script src="../web component/dashboardmenu.js"></script>
+</head>
+<body class="g-sidenav-shown bg-gray-300">
+  <!-- menu dashboard -->
+  <!-- Akhir Menu Dashboard -->
+
+
+  <!-- Core JS -->
+  <script src="../assets/js/core/popper.min.js"></script>
+  <script src="../assets/js/core/bootstrap.min.js"></script>
+  <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
+  <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+  <!-- Control Center for Material Dashboard: -->
+  <script src="../assets/js/material-dashboard.min.js?v=3.0.2"></script>
+</body>
+</html>
