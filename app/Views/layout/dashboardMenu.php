b1f6b873c293a35edcabf4ce94dc6c523cfbb5de
Amar Maruf <amarmaruf2403@gmail.com>
2022-06-05 19:08:51 +0700

    migration file ke structure file Codeigniter

---
+++ app/Views/layout/dashboardMenu.php
@@ -0,0 +1,89 @@
+<aside
+      class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 bg-gradient-dark"
+      id="sidenav-main"
+    >
+      <div class="sidenav-header">
+        <i
+          class="fas fa-times p-3 cursor-pointer text-white opacity-5 position-absolute end-0 top-0 d-none d-xl-none"
+          aria-hidden="true"
+          id="iconSidenav"
+        ></i>
+        <a
+          class="navbar-brand m-0"
+          href=" https://demos.creative-tim.com/material-dashboard/pages/dashboard "
+          target="_blank"
+        >
+          <img
+            src="./assets/img/logo-ct.png"
+            class="navbar-brand-img h-100"
+            alt="main_logo"
+          />
+          <span class="ms-1 font-weight-bold text-white">PKH BANSOS</span>
+        </a>
+      </div>
+      <hr class="horizontal light mt-0 mb-2" />
+      <div class="collapse navbar-collapse w-auto" id="sidenav-collapse-main">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link text-white active bg-gradient-primary" href="">
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-brands fa-delicious fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Dashboard</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="<?= base_url('') ?>" >
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-solid fa-database fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Data Penduduk</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="<?= base_url('') ?>">
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-solid fa-table fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Data Criteria</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="<?= base_url('') ?>">
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-solid fa-layer-group fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Metode C4.5</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="<?= base_url('') ?>">
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-solid fa-layer-group fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Metode K - Means</span>
+            </a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-white" href="<?= base_url('') ?>">
+              <div
+                class="text-white text-center me-2 d-flex align-items-center justify-content-center"
+              >
+                <i class="fa-solid fa-square-poll-vertical fa-lg"></i>
+              </div>
+              <span class="nav-link-text ms-1">Performance</span>
+            </a>
+          </li>
+        </ul>
+      </div>
+    </aside>
---
+++ app/Views/layout/page_layout.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link
+    rel="apple-touch-icon"
+    sizes="76x76"
+    href="<?= base_url('assets/img/apple-icon.png') ?>"
+  />
+  <link rel="icon" type="image/png" href="<?= base_url('assets/img/favicon.png') ?>" />
+  <!-- font and icons -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700"
+    rel="stylesheet"
+  />
+  <!-- font Awesome Icons -->
+  <script
+    src="https://kit.fontawesome.com/17bdaf7867.js"
+    crossorigin="anonymous"
+  ></script>
+  <!-- material Icons -->
+  <link
+    href="https://fonts.googleapis.com/icon?family=Material+Icons+Round"
+    rel="stylesheet"
+  />
+  <!-- CSS Files -->
+  <link
+    id="pagestyle"
+    href="<?= base_url('assets/css/material-dashboard.css?v=3.0.2') ?>"
+    rel="stylesheet"
+  />
+  <link rel="stylesheet" href="<?= base_url('assets/css/style.css') ?>">
+  <title>Halaman Dashboard | PKH C.45 dengan K - Means</title>
+</head>
+<body class="g-sidenav-show bg-gray-300 g-sidenav-pinned">
+  <!-- dashboard Menu -->
+    <?= $this->include('layout/dashboardMenu') ?>
+  <!-- Akhir Dashboard Menu -->
+  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg">
+    <!-- navbar -->
+      <?= $this->include('layout/navbar') ?>
+    <!-- Akhir navbar -->
+    <!-- Contain -->
+      <?= $this->renderSection('content') ?>
+    <!-- Akhir Contain -->
+  </main>
+
+  <!--   Core JS Files   -->
+  <script src="<?= base_url('assets/js/core/popper.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/core/bootstrap.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/plugins/perfect-scrollbar.min.js') ?>"></script>
+    <script src="<?= base_url('assets/js/plugins/perfect-scrollbar.min.js') ?>"></script>
+
+    <!-- Control Center for Material Dashboard:  -->
+    <script src="<?= base_url('assets/js/material-dashboard.min.js?v=3.0.2') ?>"></script>
+</body>
+</html>
