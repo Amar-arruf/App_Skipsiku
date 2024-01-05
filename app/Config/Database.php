737e6466387c45d8ea15124a39c106bff7e540a1
Amar Maruf <amarmaruf2403@gmail.com>
2024-01-05 13:49:32 +0700

    konfigurasi database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -35,7 +35,7 @@ class Database extends Config
         'hostname' => 'localhost',
         'username' => 'root',
         'password' => '',
-        'database' => 'spk_pkh',
+        'database' => '',
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
